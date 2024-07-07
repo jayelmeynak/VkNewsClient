f7baa355bcee95439f4c8e57ea823c232a8664f9
jayelmeynak <magomed110301@yandex.ru>
2024-07-08 00:43:41 +0300

    Create postBody and postFooter

--- app/src/main/java/com/example/vknewsclient/PostCard.kt
+++ app/src/main/java/com/example/vknewsclient/PostCard.kt
@@ -1,11 +1,13 @@
 package com.example.vknewsclient
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -21,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,39 +31,113 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PostCard(topPadding: PaddingValues) {
-    Card(modifier = Modifier.padding(top = topPadding.calculateTopPadding()),
+    Card(
+        modifier = Modifier.padding(top = topPadding.calculateTopPadding()),
         shape = RoundedCornerShape(5.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically) {
-            Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(id = R.drawable.post_comunity_thumbnail),
-                contentDescription = null
-            )
-            Column(modifier = Modifier
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            PostBody()
+            Spacer(modifier = Modifier.height(8.dp))
+            PostFooter()
+        }
+    }
+}
+
+@Composable
+fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+            contentDescription = null
+        )
+        Column(
+            modifier = Modifier
                 .weight(1f)
-                .padding(start = 8.dp)) {
-                Text(text = "dev/null", color = MaterialTheme.colorScheme.onPrimary)
-                Spacer(modifier = Modifier.padding(4.dp))
-                Text(text = "14:00", color = MaterialTheme.colorScheme.onSecondary)
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSecondary
-            )
+                .padding(start = 8.dp)
+        ) {
+            Text(text = "dev/null", color = MaterialTheme.colorScheme.onPrimary)
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(text = "14:00", color = MaterialTheme.colorScheme.onSecondary)
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Composable
+fun PostBody() {
+    Text(
+        text = "LoremIpsum", color = MaterialTheme.colorScheme.onPrimary
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    Image(
+        modifier = Modifier.fillMaxWidth(),
+        painter = painterResource(id = R.drawable.post_content_image),
+        contentDescription = null,
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+
+@Composable
+fun PostFooter() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "916")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth().weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "7")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "8")
+            IconWithText(iconResId = R.drawable.ic_like, text = "23")
         }
 
     }
 }
 
-@Preview
 @Composable
-fun Test(){
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(text = text, modifier = Modifier.padding(start = 4.dp))
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun Test() {
     PostCard(PaddingValues(32.dp))
 }
